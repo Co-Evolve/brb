dbcb8737a152d8a4557ec8df1e4bb269f49f871b
dmarzoug <dries.marzougui@gmail.com>
2023-09-08 13:41:09 +0200

    tmp safe

--- brb/seahorse/morphology_tmp.py
+++ brb/seahorse/morphology_tmp.py
@@ -50,6 +50,8 @@ class MJCSeahorseMorphology(MJCMorphology):
                                             "/v1"
                                             "/original")
 
+
+
         vs_height = 0.052
         vs_width = 0.059
         vd_height = 0.055
@@ -68,14 +70,29 @@ class MJCSeahorseMorphology(MJCMorphology):
                 euler2quat(*[np.pi/2, 0.0, np.pi]),
                 euler2quat(*[np.pi/2, 0, np.pi/2])
                 ]
-        positions = [
-                [-vs_height, 0, 0],
+        positions = np.array([
+                [-vs_height, 0.002, 0],
                 [0, vd_width, 0],
                 [dd_height, 0, 0],
                 [0, -ds_width, 0]
-                ]
+                ])
 
-        targets = [0, 1, 2, 3]
+        # Shift z dim of plates such that their middle is at z = 0
+        #   todo: need real measurement
+        plate_depth = 0.015
+        positions[:, 2] = -plate_depth / 2
+        #
+        # # Shift x and y dim of plates such that they are locked in gliding joints
+        # #   todo: need real measurements
+        plate_y_gliding_shift = 0.0085 / 2
+        plate_y_gliding_shift_signed = np.array([1, -1, -1, 1]) * plate_y_gliding_shift
+        plate_x_gliding_shift = 0.0085 / 2
+        plate_x_gliding_shift_signed = np.array([1, 1, -1, -1]) * plate_x_gliding_shift
+        positions[:, 1] += plate_y_gliding_shift_signed
+        positions[:, 0] += plate_x_gliding_shift_signed
+
+
+        targets = [0, 1]
         for plate_index, plate in enumerate(plates):
             if plate_index not in targets:
                 continue
@@ -96,15 +113,26 @@ class MJCSeahorseMorphology(MJCMorphology):
                     quat=rotations[plate_index]
                     )
 
-        self.mjcf_body.add('geom',
-                           type='box',
-                           size=[10, 0.0001, 0.0001],
-                           rgba=[1, 0, 0, 0.5])
-        self.mjcf_body.add('geom',
-                           type='box',
-                           size=[0.0001, 10, 0.0001],
-                           rgba=[0, 1, 0, 0.5])
+        self.mjcf_model.asset.add(
+                "mesh", name="vertebrae", file=f"vertebrae.STL", scale=[0.001, 0.001, 0.001]
+                )
+        self.mjcf_body.add(
+                'geom',
+                name=f"vertebrae",
+                type="mesh",
+                mesh=f"vertebrae",
+                pos=[0, 0, 0],
+                )
 
+        # self.mjcf_body.add('geom',
+        #                    type='box',
+        #                    size=[10, 0.0001, 0.0001],
+        #                    rgba=[1, 0, 0, 0.5])
+        # self.mjcf_body.add('geom',
+        #                    type='box',
+        #                    size=[0.0001, 10, 0.0001],
+        #                    rgba=[0, 1, 0, 0.5])
+        #
 
 if __name__ == '__main__':
     morphology_specification = default_seahorse_morphology_specification(num_segments=5)
