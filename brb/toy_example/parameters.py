86552f279e95896873558b662eda30f39aedee75
dmarzoug <dries.marzougui@gmail.com>
2023-10-16 14:41:31 +0200

    use parameterizer

--- brb/toy_example/parameters.py
+++ brb/toy_example/parameters.py
@@ -1,26 +1,50 @@
-from fprs.parameters import ContinuousParameter, FixedParameter
+from typing import List
+
+from fprs.parameters import ContinuousParameter
+from fprs.specification import MorphologySpecificationParameterizer
 
 from brb.toy_example.morphology.specification.default import default_toy_example_morphology_specification
+from brb.toy_example.morphology.specification.specification import ToyExampleMorphologySpecification
+
+
+class ToyExampleMorphologySpecificationParameterizer(MorphologySpecificationParameterizer):
+    def __init__(
+            self,
+            torso_radius_range: tuple[float, float]
+            ) -> None:
+        super().__init__()
+        self._torso_radius_range = torso_radius_range
+
+    def parameterize_specification(
+            self,
+            specification: ToyExampleMorphologySpecification
+            ) -> None:
+        # Passing None as value will set the parameter to a random value within the given range
+        morphology_specification.torso_specification.radius = ContinuousParameter(
+                low=self._torso_radius_range[0], high=self._torso_radius_range[1], value=None
+                )
+
+    def get_parameter_labels(
+            self,
+            specification: ToyExampleMorphologySpecification
+            ) -> List[str]:
+        return ["torso-radius"]
+
 
 if __name__ == '__main__':
     morphology_specification = default_toy_example_morphology_specification(
             num_arms=4, num_segments_per_arm=3
             )
 
-    # Passing None as value will set the parameter to a random value within the given range
-    morphology_specification.torso_specification.radius = ContinuousParameter(
-            low=0.1, high=0.6, value=None
-            )
+    parameterizer = ToyExampleMorphologySpecificationParameterizer(torso_radius_range=(0.05, 0.15))
+    parameterizer.parameterize_specification(specification=morphology_specification)
 
-    all_parameters = morphology_specification.parameters
-    non_fixed_parameters = list(
-            filter(
-                    lambda
-                        parameter: not isinstance(parameter, FixedParameter), all_parameters
-                    )
-            )
     print("All parameters:")
-    print(f"\t{all_parameters}")
+    print(f"\t{morphology_specification.parameters}")
     print()
     print("Parameters to optimise:")
-    print(f"\t{non_fixed_parameters}")
+    for parameter, label in zip(
+            parameterizer.get_target_parameters(specification=morphology_specification),
+            parameterizer.get_parameter_labels(specification=morphology_specification)
+            ):
+        print(f"\t{label}\t->\t{parameter}")
